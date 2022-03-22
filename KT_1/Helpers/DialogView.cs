d4c473100af412780ff0902a69f3e3a948ec9b94
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 21:36:18 +0300

    Add DialogView interface

---
+++ KT_1/Helpers/DialogView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.DataAccessLayer
+{
+    public interface DialogView<T>
+    {
+        bool? ShowDialog(T viewModel);
+    }
+}
