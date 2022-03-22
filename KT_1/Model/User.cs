0ec91a5517c89af37d2d4c13a143d13cdf6168c5
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 10:54:43 +0300

    Add User domain entity

---
+++ KT_1/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.Model
+{
+    internal class User
+    {
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public string Name { get; private set; }
+        public string Role { get; private set; }
+
+        
+        public User(string login, string password, string name, string role)
+        {
+            Login = login;
+            Password = password;
+            Name = name;
+            Role = role;
+        }
+    }
+}
