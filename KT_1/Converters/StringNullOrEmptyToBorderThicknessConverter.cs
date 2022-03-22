5bf14b6fc3a0679daea42565caf5d6393b028fdf
prtname <91187493+prtname@users.noreply.github.com>
2022-03-23 00:51:52 +0300

    Add StringNullOrEmptyToBorderThicknessConverter

---
+++ KT_1/Converters/StringNullOrEmptyToBorderThicknessConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace KT_1.Converters
+{
+    internal class StringNullOrEmptyToBorderThicknessConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return String.IsNullOrEmpty(value as string) ? 1 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return null;
+        }
+    }
+}
