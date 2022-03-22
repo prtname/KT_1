8297f74b0f5b49fa38b3a714495245c6f1a41856
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 11:13:02 +0300

    Add products repository

---
+++ KT_1/DataAccessLayer/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using KT_1.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.DataAccessLayer
+{
+    internal class ProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return new List<Product>(m_Products);
+        }
+
+        public void AddProduct(Product product)
+        {
+            m_Products.Add(product);
+        }
+
+        public void AddRangeProducts(IEnumerable<Product> products)
+        {
+            m_Products.AddRange(products);
+        }
+
+
+        private List<Product> m_Products = new List<Product>();
+    }
+}
