5c0ff78cf443674de4a8862955c242c92c2c1281
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 16:50:37 +0300

    Add RelayCommand class

---
+++ KT_1/Helpers/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KT_1.Helpers
+{
+    internal class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute)
+            : this(null, execute)
+        {
+        }
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            m_CanExecute = canExecute;
+            m_Execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return m_CanExecute == null || m_CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            m_Execute(parameter);
+        }
+        
+
+        private Action<object> m_Execute;
+        private Predicate<object> m_CanExecute;
+    }
+}
