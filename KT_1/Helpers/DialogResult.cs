a2ecd99d5de15a9e0bf667ee883668797e2d8387
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 21:15:48 +0300

    Add dialog results enum

---
+++ KT_1/Helpers/DialogResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.Helpers
+{
+    internal enum DialogResult
+    {
+        None,
+        Cancel,
+        Yes,
+        No
+    }
+}
