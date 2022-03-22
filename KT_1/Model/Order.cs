cba19a3fa6a834e4a95a10bb42a79c6ddeefa69a
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 10:58:17 +0300

    Add Order domain entity

---
+++ KT_1/Model/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.Model
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int NumberOfProducts { get; set; }
+        public DateTime Date { get; set; }
+        public string ProcessStatus { get; set; }
+        public User Customer { get; set; }
+        public User Manager { get; set; }
+        public double Price { get; set; }
+
+
+        public Order(int id, Product product, int numberOfProducts, DateTime date, 
+            string processStatus, User customer, User manager, double price)
+        {
+            Id = id;
+            Product = product;
+            NumberOfProducts = numberOfProducts;
+            Date = date;
+            ProcessStatus = processStatus;
+            Customer = customer;
+            Manager = manager;
+            Price = price;
+        }
+    }
+}
