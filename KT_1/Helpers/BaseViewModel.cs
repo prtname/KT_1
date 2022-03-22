529b2ad1e53c39e5c37637a567d0311945da867a
prtname <91187493+prtname@users.noreply.github.com>
2022-03-22 16:51:31 +0300

    Add BaseViewModel class

---
+++ KT_1/Helpers/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace KT_1.Helpers
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
