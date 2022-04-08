b5a96a6aaa2f4f1bd1537be81c1acecde8b92243
prtname <91187493+prtname@users.noreply.github.com>
2022-04-08 14:18:49 +0300

    Add MainViewFactory

---
+++ KT_1/View/Factories/MainViewFactory.cs
@@ -0,0 +1,33 @@
+﻿using KT_1.Model;
+using KT_1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.View.Factories
+{
+    public class MainViewFactory
+    {
+        public event EventHandler Closed;
+
+        public MainViewFactory(User currentUser)
+        {
+            m_CurrentUser = currentUser;
+        }
+
+        public void Show()
+        {
+            var viewModel = new MainViewModel(m_CurrentUser);
+            var view = new MainWindow(viewModel);
+
+            view.Closed += Closed;
+
+            view.Show();
+        }
+
+
+        private User m_CurrentUser;
+    }
+}
