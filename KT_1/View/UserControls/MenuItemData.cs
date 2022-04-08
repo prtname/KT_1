e53699a5af979e5c386743a683d918bab878b3f4
prtname <91187493+prtname@users.noreply.github.com>
2022-04-08 14:57:23 +0300

    Add navigation bar user control

---
+++ KT_1/View/UserControls/MenuItemData.cs
@@ -0,0 +1,30 @@
+﻿using KT_1.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.View.UserControls
+{
+    public class MenuItemData
+    {
+        public string Title { get; private set; }
+        public Uri PageUri { get; private set; }
+        public RelayCommand ClickCommand { get; private set; }
+
+
+        public MenuItemData(string title, Uri pageUri)
+        {
+            Title = title;
+            PageUri = pageUri;
+        }
+
+        public MenuItemData(string title, RelayCommand clickCommand)
+        {
+            Title = title;
+            ClickCommand = clickCommand;
+
+        }
+    }
+}
---
+++ KT_1/View/UserControls/NavigationBar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KT_1.View.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для NavigationBar.xaml
+    /// </summary>
+    public partial class NavigationBar : UserControl
+    {
+        public static readonly DependencyProperty ItemsDependencyProperty = DependencyProperty.Register(nameof(Items), typeof(IEnumerable<MenuItemData>), typeof(NavigationBar));
+
+        public IEnumerable<MenuItemData> Items
+        {
+            get => (IEnumerable<MenuItemData>)GetValue(ItemsDependencyProperty);
+            set => SetValue(ItemsDependencyProperty, value);
+        }
+        public NavigationBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
