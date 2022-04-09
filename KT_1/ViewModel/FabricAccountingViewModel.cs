29803a10a1b17840da715d332057284556edfece
prtname <91187493+prtname@users.noreply.github.com>
2022-04-09 18:31:28 +0300

    Add page views and viewmodels

---
+++ KT_1/ViewModel/FabricAccountingViewModel.cs
@@ -0,0 +1,45 @@
+﻿using KT_1.DataAccessLayer;
+using KT_1.Helpers;
+using KT_1.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.ViewModel
+{
+    public class FabricAccountingViewModel : BaseViewModel
+    {
+        public ObservableCollection<Fabric> Fabrics 
+        {
+            get { return m_Fabrics; } 
+            private set
+            {
+                m_Fabrics = value;
+                OnPropertyChanged("Fabrics");
+            }
+        }
+
+        public FabricAccountingViewModel(FabricRepository fabricRepository)
+        {
+            m_FabricRepository = fabricRepository;
+            m_Fabrics = new ObservableCollection<Fabric>(m_FabricRepository.GetAllProducts());
+        }
+
+        private void OnFabricsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    m_FabricRepository.AddRange((IEnumerable<Fabric>)e.NewItems);
+                    break;
+            }
+        }
+
+        private FabricRepository m_FabricRepository;
+        private ObservableCollection<Fabric> m_Fabrics;
+    }
+}
---
+++ KT_1/ViewModel/ProductAccountingViewModel.cs
@@ -0,0 +1,29 @@
+﻿using KT_1.DataAccessLayer;
+using KT_1.Helpers;
+using KT_1.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.ViewModel
+{
+    public class ProductAccountingViewModel : BaseViewModel
+    {
+        public ObservableCollection<Product> Products
+        {
+            get { return m_Products; }
+        }
+
+        public ProductAccountingViewModel(ProductRepository productRepository)
+        {
+            m_ProductRepository = productRepository;
+            m_Products = new ObservableCollection<Product>(m_ProductRepository.GetAllProducts());
+        }
+
+        private ObservableCollection<Product> m_Products;
+        private ProductRepository m_ProductRepository;
+    }
+}
