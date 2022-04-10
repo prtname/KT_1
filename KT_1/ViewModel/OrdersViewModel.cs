8d6514cf9cdc4343058c1f584fd2f60fdd9584c4
prtname <91187493+prtname@users.noreply.github.com>
2022-04-10 10:04:16 +0300

    Replace CustomerOrders to Orders view model

--- KT_1/ViewModel/OrdersViewModel.cs
+++ KT_1/ViewModel/OrdersViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using KT_1.DataAccessLayer;
+using KT_1.Helpers;
+using KT_1.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KT_1.ViewModel
 {
-    public class OrdersViewModel
+    public class OrdersViewModel : BaseViewModel
     {
+        public ObservableCollection<Order> Orders => m_Orders;
+
+        public OrdersViewModel(OrderRepository orderRepository, User userOrders)
+        {
+            m_OrderRepository = orderRepository;
+            m_UserOrders = userOrders;
+
+            m_Orders = new ObservableCollection<Order>(m_OrderRepository.GetOrdersForUser(m_UserOrders));
+        }
+
+        public OrdersViewModel(OrderRepository orderRepository)
+        {
+            m_OrderRepository = orderRepository;
+            m_Orders = new ObservableCollection<Order>(m_OrderRepository.GetAllOrders());
+        }
+
+        private OrderRepository m_OrderRepository;
+        private ObservableCollection<Order> m_Orders;
+
+        private User m_UserOrders;
+
     }
 }
