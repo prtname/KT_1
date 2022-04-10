02cf6a0c5915bbfff6c22914bc3cea6f2af0e280
prtname <91187493+prtname@users.noreply.github.com>
2022-04-10 10:02:33 +0300

    Add AccessoryBatch and FabricRoll models

---
+++ KT_1/Model/AccessoryBatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.Model
+{
+    public class AccessoryBatch
+    {
+        public string BatchArticul { get; set; }
+        public int AccessoryNumber { get; set; }
+        public Accessory Accessory { get; set; }
+
+        public AccessoryBatch(string articul, int number, Accessory accessory)
+        {
+            BatchArticul = articul;
+            AccessoryNumber = number;
+            Accessory = accessory;
+        }
+    }
+}
---
+++ KT_1/Model/FabricRoll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_1.Model
+{
+    public class FabricRoll
+    {
+        public string RollArticul { get; set; }
+        public double Length { get; set; }
+        public Fabric Fabric { get; set; }
+
+
+        public FabricRoll(string articul, double length, Fabric fabric)
+        {
+            RollArticul = articul;
+            Length = length;
+            Fabric = fabric;
+        }
+    }
+}
