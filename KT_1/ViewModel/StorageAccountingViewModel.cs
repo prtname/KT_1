6b9fc18bf9ff6850b71da0ebf812c3f7f3837b57
prtname <91187493+prtname@users.noreply.github.com>
2022-04-10 16:27:04 +0300

    Implement StorageAccouningViewModel

--- KT_1/ViewModel/StorageAccountingViewModel.cs
+++ KT_1/ViewModel/StorageAccountingViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using KT_1.DataAccessLayer;
+using KT_1.Helpers;
+using KT_1.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KT_1.ViewModel
 {
-    public class StorageAccountingViewModel
+    public class StorageAccountingViewModel : BaseViewModel
     {
+        public ObservableCollection<FabricRoll> FabricRolls => m_FabricRolls;
+        public ObservableCollection<AccessoryBatch> AccessoryBatches => m_AccessoryBatches;
+
+        public StorageAccountingViewModel(FabricRepository fabricRepository, AccessoryRepository accessoryRepository)
+        {
+            m_FabricRepository = fabricRepository;
+            m_AccessoryRepository = accessoryRepository;
+
+            m_FabricRolls = new ObservableCollection<FabricRoll>(m_FabricRepository.GetAllRolls());
+            m_AccessoryBatches = new ObservableCollection<AccessoryBatch>(m_AccessoryRepository.GetAllBatches());
+        }
+
+
+        private ObservableCollection<FabricRoll> m_FabricRolls;
+        private ObservableCollection<AccessoryBatch> m_AccessoryBatches;
+
+        private FabricRepository m_FabricRepository;
+        private AccessoryRepository m_AccessoryRepository;
     }
 }
